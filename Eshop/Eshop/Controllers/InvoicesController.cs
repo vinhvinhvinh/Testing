5bd4c8adb645a6927b822b934ecec6811baa94d1
AnhNhat111 <0306191344@caothang.edu.vn>
2022-01-01 01:40:31 +0700

    model invoice + crud

---
+++ Eshop/Eshop/Controllers/InvoicesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Eshop.Data;
+using Eshop.Models;
+
+namespace Eshop.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        private readonly EshopContext _context;
+
+        public InvoicesController(EshopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Invoices
+        public async Task<IActionResult> Index()
+        {
+            var eshopContext = _context.Invoice.Include(i => i.Account);
+            return View(await eshopContext.ToListAsync());
+        }
+
+        // GET: Invoices/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoice
+                .Include(i => i.Account)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // GET: Invoices/Create
+        public IActionResult Create()
+        {
+            ViewData["AccountId"] = new SelectList(_context.Account, "Id", "Address");
+            return View();
+        }
+
+        // POST: Invoices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Code,AccountId,IssuedDate,ShippingAddress,ShippingPhone,Total,Status")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(invoice);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Account, "Id", "Address", invoice.AccountId);
+            return View(invoice);
+        }
+
+        // GET: Invoices/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoice.FindAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            ViewData["AccountId"] = new SelectList(_context.Account, "Id", "Address", invoice.AccountId);
+            return View(invoice);
+        }
+
+        // POST: Invoices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Code,AccountId,IssuedDate,ShippingAddress,ShippingPhone,Total,Status")] Invoice invoice)
+        {
+            if (id != invoice.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(invoice);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InvoiceExists(invoice.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Account, "Id", "Address", invoice.AccountId);
+            return View(invoice);
+        }
+
+        // GET: Invoices/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoice
+                .Include(i => i.Account)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // POST: Invoices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var invoice = await _context.Invoice.FindAsync(id);
+            _context.Invoice.Remove(invoice);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InvoiceExists(int id)
+        {
+            return _context.Invoice.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Eshop/Eshop/Models/Invoice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Eshop.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Mã HĐ")]
+        public string Code { get; set; }
+
+        public int AccountId { get; set; }
+
+        // Navigation reference property cho khóa ngoại đến Account 
+        [DisplayName("Khách hàng")]
+        public Account Account { get; set; }
+
+        [DisplayName("Ngày lập")]
+        [DataType(DataType.DateTime)]
+        public DateTime IssuedDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Địa chỉ giao hàng")]
+        public string ShippingAddress { get; set; }
+
+        [DisplayName("SĐT giao hàng")]
+        public string ShippingPhone { get; set; }
+
+        [DisplayName("Tổng tiền")]
+        [DisplayFormat(DataFormatString = "{0:n0}")]
+        [DefaultValue(0)]
+        public int Total { get; set; } = 0;
+
+        [DisplayName("Còn hiệu lực")]
+        [DefaultValue(true)]
+        public bool Status { get; set; } = true;
+
+        // Collection reference property cho khóa ngoại từ InvoiceDetail
+        //public List<InvoiceDetail> InvoiceDetails { get; set; }
+    }
+}
