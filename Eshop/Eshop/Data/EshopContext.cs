764138b97f6a828f881e09a94b36bcaa8b4d1a02
Vinh <ntvinh0606@gmail.com>
2021-12-31 13:54:39 +0700

    Vinh làm Model Account, AccountController with View Account

---
+++ Eshop/Eshop/Data/EshopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Eshop.Models;
+
+namespace Eshop.Data
+{
+    public class EshopContext : DbContext
+    {
+        public EshopContext (DbContextOptions<EshopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Eshop.Models.Account> Account { get; set; }
+    }
+}
