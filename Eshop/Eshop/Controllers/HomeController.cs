b47d197273b61665cd394c36af9fe0bd559cd026
Vinh <ntvinh0606@gmail.com>
2022-01-07 02:55:17 +0700

    Xong đăng nhập,đăng ký, đăng xuất, hiện Cake Categories ở Home

--- Eshop/Eshop/Controllers/HomeController.cs
+++ Eshop/Eshop/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace Eshop.Controllers
 {
     public class HomeController : Controller
     {
-        
+
         private readonly EshopContext _context;
 
         public HomeController(EshopContext context)
@@ -30,6 +30,9 @@ namespace Eshop.Controllers
                 ViewBag.Fullname = HttpContext.Request.Cookies["AccountName"].ToString();
                 ViewBag.Avatar = HttpContext.Request.Cookies["AccountAvatar"].ToString();
             }
+
+            var ptype = _context.ProductType.ToList();
+            ViewBag.AllType = ptype;
             return View();
         }
 
@@ -72,7 +75,7 @@ namespace Eshop.Controllers
 
                 if (!login.IsAdmin)
                 {
-                        return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Home");
                 }
                 else
                 {
@@ -95,5 +98,59 @@ namespace Eshop.Controllers
             return RedirectToAction("Index", "Home");
 
         }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(string username, string email, string phone, string address, string fullname, string password, string confirmpassword)
+        {
+
+            // Kiểm tra đã tồn tại username nào hay chưa ?!
+            Account signUpAccount = _context.Account.Where(a => a.Username == username).FirstOrDefault();
+            if (signUpAccount == null)
+            {
+                if (password != confirmpassword)
+                {
+                    ViewBag.ConfirmPasswordError = "The password confirmation does not match";
+                    return View();
+                }
+
+                Account newUserSignUp = new Account();
+                newUserSignUp.Username = username;
+                newUserSignUp.Password = password;
+                newUserSignUp.Email = email;
+                newUserSignUp.Phone = phone;
+                newUserSignUp.Address = address;
+                newUserSignUp.Fullname = fullname;
+                newUserSignUp.IsAdmin = false;
+                newUserSignUp.Avatar = "";
+                newUserSignUp.Status = 1;
+                _context.Account.Add(newUserSignUp);
+                _context.SaveChanges();
+
+                Account login = _context.Account.Where(a => a.Username == username).FirstOrDefault();
+                CookieOptions cookieDate = new CookieOptions()
+                {
+                    Expires = DateTime.Now.AddDays(30)
+                    //Expires = DateTime.UtcNow.AddMilliseconds(1500)
+                };
+
+                HttpContext.Response.Cookies.Append("AccountId", login.Id.ToString(), cookieDate);
+                HttpContext.Response.Cookies.Append("AccountName", login.Fullname.ToString(), cookieDate);
+                HttpContext.Response.Cookies.Append("AccountAvatar", login.Avatar.ToString(), cookieDate);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.SignUpUsernameFailMessage = "Username has already been taken.";
+                return View();
+            }
+
+        }
+
     }
 }
